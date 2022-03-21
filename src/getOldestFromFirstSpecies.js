18ca13cdc849c6867d924e4f78044bebfc4777b0
allanzaparoli <allanzaparoli@gmail.com>
2022-03-21 15:58:47 -0300

    questao 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
-
+const { employees } = data;
+const { species } = data;
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const idFuncionario = employees.find((element) => element.id === id);
+  const idAnimal = idFuncionario.responsibleFor[0];
+  const especies = species.find((element) => element.id === idAnimal);
+  const animais = especies.residents;
+  const IdadeAnimais = animais.map((element) => element.age);
+  const animalVelho = Math.max(...IdadeAnimais);
+  const dadosAnimalVelho = animais.find((element) => element.age === animalVelho);
+  return [dadosAnimalVelho.name, dadosAnimalVelho.sex, dadosAnimalVelho.age];
 }
-
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 module.exports = getOldestFromFirstSpecies;
